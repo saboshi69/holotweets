560b87e233ffbeaf2cdc2dbcbd925ae603801857
Samuel <42663794+saboshi69@users.noreply.github.com>
2021-03-26 15:01:02 +0800

    set query hook for mobile device

--- src/container/SettingContextProvider/index.jsx
+++ src/container/SettingContextProvider/index.jsx
@@ -1,4 +1,5 @@
 import { Box } from '@chakra-ui/layout';
+import { useMediaQuery } from "@chakra-ui/react";
 import React, { createContext, useState, useEffect, useContext } from 'react';
 
 
@@ -14,9 +15,15 @@ export function useFollowing() {
 }
 
 function SettingContextProvider({ children }) {
+  const [isLargeDevice] = useMediaQuery(["(min-width: 600px)"]);
+
   const [isInit, setIsInit] = useState(true);
-  const [config, setConfig] = useState({
+  const [config, setConfig] = useState(isLargeDevice ? {
     col: 4,
+    row: 1,
+    isWrap: true,
+  } : {
+    col: 1,
     row: 1,
     isWrap: true,
   });
