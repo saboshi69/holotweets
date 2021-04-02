3365badb5323bfb9977c287ae0e50be86cbb62f4
Samuel <42663794+saboshi69@users.noreply.github.com>
2021-04-02 21:23:19 +0800

    add google Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,25 @@
-import { ChakraProvider } from "@chakra-ui/react"
+import { ChakraProvider } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import * as gtag from '../../lib/gtag';
+import React, { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = url => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <ChakraProvider>
       <Component {...pageProps} />
     </ChakraProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
